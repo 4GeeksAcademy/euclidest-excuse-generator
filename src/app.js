5b827c3d51199913eaafbc74f868f4eec7b49459
EuclidesT <94712951+EuclidesT@users.noreply.github.com>
2024-01-05 00:22:30 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuses").innerHTML = generateExcuses();
+  });
+  console.log("Hello Rigo from the console");
+};
+
+let generateExcuses = () => {
+  let pronoun = ["Yo", "Ellos", "Nosotros", "Ustedes"];
+  let subject = ["Piloto", "Doctor", "Ardilla", "Cama"];
+  let action = [
+    "Respirar",
+    "Ya tengo el bolso",
+    "Acostado en el mueble",
+    "Deporte"
+  ];
+  let posseition = ["vehículo", "Hogar", "Refugio", "Estanque"];
+  let where = ["en la avenida", "en el trabajo", "en la calle", "en el piso"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let posseitionIndx = Math.floor(Math.random() * posseition.length);
+  let whereIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subjIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    posseition[posseitionIndx] +
+    " " +
+    where[whereIndx]
+  );
 };
